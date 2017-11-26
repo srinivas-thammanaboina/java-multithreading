e390ecfea5700c8dd9be8ecf16a758371203be26
Srinivas <Srinivasthammana99@gmail.com>
2017-11-26 02:05:30 -0700

    adding Runnable thread class

---
+++ basciconcurrency/com/srinithread/concurencybasic/RunnableApp.java
@@ -0,0 +1,40 @@
+package com.srinithread.concurencybasic;
+
+
+class Runner1 implements Runnable {
+
+	@Override
+	public void run() {
+
+		for(int i=0; i<=10;++i){
+			System.out.println("Runner 1 -> "+i);
+		}
+	}
+}
+
+class Runner2 implements Runnable {
+
+	@Override
+	public void run() {
+
+		for(int i=0; i<=10;++i){
+			System.out.println("Runner 2 -> "+i);
+		}
+	}
+}
+
+
+public class RunnableApp {
+
+	public static void main(String[] args) {
+
+		Thread t1 = new Thread(new Runner1());
+		
+		Thread t2 = new Thread(new Runner2());
+		
+		t1.start();
+		
+		t2.start();
+	}
+
+}
